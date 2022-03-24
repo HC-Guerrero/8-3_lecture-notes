5cac73daf0ae7d60b1384ccbc1907db6c9562891
H.C.Guerrero <clydeguerrero1@gmail.com>
2022-03-24 17:10:47 -0300

    update

---
+++ ReadingDoms.js
@@ -0,0 +1,21 @@
+/*{
+  let stringTonumberConver = '';
+  window.prompt('Enter a number before you continue', stringTonumberConver);
+
+  alert(Number(stringTonumberConver * 2));
+} */
+const userNumber = prompt('Enter a number');
+if (isNaN(userNumber)) {
+  alert('Please enter a valid number!');
+} else {
+  alert(`Double your number is ${userNumber * 2}`);
+}
+{
+  console.log(`I'm at the top of the world, Baby!`);
+}
+/* while (true) {
+  console.log('You dun goofed');
+} */
+/*{
+  alert('welcome to my page!');
+} */
