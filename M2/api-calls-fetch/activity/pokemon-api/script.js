123984228b572275d74c5ee0f614fc3a403181f1
H.C.Guerrero <clydeguerrero1@gmail.com>
2022-04-06 11:07:10 -0300

    4/6/22

--- M2/api-calls-fetch/activity/pokemon-api/script.js
+++ M2/api-calls-fetch/activity/pokemon-api/script.js
@@ -2,12 +2,12 @@ const BASE_URL = 'https://pokeapi.co/api/v2/pokemon';
 
 const form = document.querySelector('form');
 
-// form.addEventListener('submit', (event) => {
-//   event.preventDefault();
-//   const { id } = event.target;
-//   getPokemonByID(id.value);
-//   id.value = '';
-// });
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+  const { id } = event.target;
+  getPokemonByID(id.value);
+  id.value = '';
+});
 //
 //
 //
@@ -15,21 +15,21 @@ const form = document.querySelector('form');
 // These notes are for reference
 // The full solution is commented out below on line 65
 // fetch api data
-fetch(BASE_URL + '/8')
-  // get json from the response body
-  .then((response) => {
-    return response.json();
-  })
-  // process json to dom updates
-  .then((json) => {
-    console.log(json);
-    // add a helper function to process the dom update
-  })
-  // catch errors
-  .catch((err) => {
-    console.log(err);
-    // add a helper function to process the dom update error msg
-  });
+//fetch(BASE_URL + '/8')
+// get json from the response body
+// .then((response) => {
+//  return response.json();
+//})
+// process json to dom updates
+//.then((json) => {
+//  console.log(json);
+// add a helper function to process the dom update
+// })
+// catch errors
+//.catch((err) => {
+//  console.log(err);
+// add a helper function to process the dom update error msg
+//  });
 //
 //
 //
@@ -62,36 +62,36 @@ fetch(BASE_URL + '/8')
 //
 //
 //
-// function getPokemonByID(id) {
-//   fetch(`${BASE_URL}/${3}`)
-//     .then((response) => response.json())
-//     .then((result) => {
-//       const pokemon = createPokemonArticle(result);
-//       document.querySelector('.pokemon').append(pokemon);
-//     })
-//     .catch((error) => {
-//       const message = createErrorMessage(error);
-//       document.querySelector('main').append(message);
-//     });
-// }
+function getPokemonByID(id) {
+  fetch(`${BASE_URL}/${id}`)
+    .then((response) => response.json())
+    .then((result) => {
+      const pokemon = createPokemonArticle(result);
+      document.querySelector('.pokemon').append(pokemon);
+    })
+    .catch((error) => {
+      const message = createErrorMessage(error);
+      document.querySelector('main').append(message);
+    });
+}
 
-// function createPokemonArticle(pokemon) {
-//   const article = document.createElement('article');
-//   article.innerHTML = `
-//     <img src="${pokemon.sprites.front_default}" alt="${pokemon.name}" />
-//     <h2>${pokemon.name} (#${pokemon.order})</h2>
-//   `;
+function createPokemonArticle(pokemon) {
+  const article = document.createElement('article');
+  article.innerHTML = `
+    <img src="${pokemon.sprites.front_default}" alt="${pokemon.name}" />
+   <h2>${pokemon.name} (#${pokemon.order})</h2>
+  `;
 
-//   return article;
-// }
+  return article;
+}
 
-// function createErrorMessage(message) {
-//   const section = document.createElement('section');
-//   section.classList.add('error');
-//   section.innerHTML = `
-//     <p>There was an error!</p>
-//     <p class="message">${message}</p>
-//   `;
+function createErrorMessage(message) {
+  const section = document.createElement('section');
+  section.classList.add('error');
+  section.innerHTML = `
+    <p>There was an error!</p>
+    <p class="message">${message}</p>
+   `;
 
-//   return section;
-// }
+  return section;
+}
